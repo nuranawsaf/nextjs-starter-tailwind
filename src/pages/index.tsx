d0d1bef727c037cf511d5cf6156caab224edf060
nuranawsaf <nuranawsaf712@gmail.com>
2022-12-05 11:20:02 +0600

    add source

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { NextPage } from 'next';
+import Image from 'next/image';
+
+import { motion } from 'framer-motion';
+
+const Home: NextPage = () => {
+  return (
+    <div className="min-h-screen w-full overflow-hidden bg-gray-50   transition-all duration-300 dark:bg-gray-900">
+      hello
+    </div>
+  );
+};
+
+export default Home;
